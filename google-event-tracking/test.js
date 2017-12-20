f3edafe466b16d91582224337a820b1567944bc6
Gabriel Ferreira <jonypepe@hotmail.com>
2017-12-20 19:47:28 +0000

    First test google event tracking

---
+++ google-event-tracking/test.js
@@ -0,0 +1,3 @@
+ga('send', 'event', 'Product/CategoryName/Centrix20', 'ClickTab', 'Software', null)
+ga('send', 'event', 'Product/CategoryName/Centrix20', 'VideoPlay', 'Software', null)
+ga('send', 'event', 'Product/CategoryName/Centrix20', 'VideoPlayFinished', '', 344)
